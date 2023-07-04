a812eba5f453ae5de5f8417d4959424f559e483d
lorenzo <105223252+Rossini-lorenzo@users.noreply.github.com>
2023-07-04 12:07:12 +0200

    pulizie di inizio progetto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,29 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CaHomeComponent } from './AppNoLogged-components/ca-home/ca-home.component';
 import { CaLoginComponent } from './AppNoLogged-components/ca-login/ca-login.component';
-import { CaRegistrationComponent } from './AppNoLogged-components/ca-registration/ca-registration.component';
-import { CaRegistrationPromoteComponent } from './AppPromote-components/ca-registration-promote/ca-registration-promote.component';
 import { CaNavbarComponent } from './AppNoLogged-components/ca-navbar/ca-navbar.component';
-import { CaNavbarLoggedComponent } from './Appuser-components/ca-navbar-logged/ca-navbar-logged.component';
-import { CaHomeLoggedComponent } from './Appuser-components/ca-home-logged/ca-home-logged.component';
-import { GoogleMapComponent } from './google-map/google-map.component';
+
 import { AuthGuard } from './AuthGuard';
-import { CaHomeLoggedPromoteComponent } from './AppPromote-components/ca-home-logged-promote/ca-home-logged-promote.component';
-import { CaGestisciAttivitaPromoteComponent } from './AppPromote-components/ca-gestisci-attivita-promote/ca-gestisci-attivita-promote.component';
+
 
 
 
 
 const routes: Routes = [
-  { path: 'registrazionePromote', component:CaRegistrationPromoteComponent },
-  { path: '', component: CaHomeComponent },
-  { path: 'registrazione', component:CaRegistrationComponent },
+  { path: '', component: CaLoginComponent },
   { path: 'login', component: CaLoginComponent},
-  { path: 'loggedHome', component: CaHomeLoggedComponent,canActivate: [AuthGuard] },
-  { path: 'loggedHomePromote', component: CaHomeLoggedPromoteComponent,canActivate: [AuthGuard] },
-  { path: 'xxx', component: CaNavbarLoggedComponent },
-  { path: 'xxx', component: GoogleMapComponent},
-  { path: 'gestisciAttavitaPromote', component: CaGestisciAttivitaPromoteComponent,canActivate: [AuthGuard] },
+
 ];
 // , canActivate: [AuthGuard]
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CaRegistrationComponent } from './AppNoLogged-components/ca-registration/ca-registration.component';
 import { CaLoginComponent } from './AppNoLogged-components/ca-login/ca-login.component';
 import { CaHomeComponent } from './AppNoLogged-components/ca-home/ca-home.component';
 import { CaNavbarComponent } from './AppNoLogged-components/ca-navbar/ca-navbar.component';
-import { CaNavbarLoggedComponent } from './Appuser-components/ca-navbar-logged/ca-navbar-logged.component';
 import { GoogleMapsModule } from '@angular/google-maps';
-import { GoogleMapComponent } from './google-map/google-map.component'
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { CaHomeLoggedComponent } from './Appuser-components/ca-home-logged/ca-home-logged.component';
-import { CaRegistrationPromoteComponent } from './AppPromote-components/ca-registration-promote/ca-registration-promote.component';
-import { CaNavbarLoggedPromoteComponent } from './AppPromote-components/ca-navbar-logged-promote/ca-navbar-logged-promote.component';
-import { CaHomeLoggedPromoteComponent } from './AppPromote-components/ca-home-logged-promote/ca-home-logged-promote.component';
-import { CaGestisciAttivitaPromoteComponent } from './AppPromote-components/ca-gestisci-attivita-promote/ca-gestisci-attivita-promote.component';
+
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
@@ -23,17 +16,10 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 @NgModule({
   declarations: [
     AppComponent,
-    CaRegistrationComponent,
     CaLoginComponent,
     CaHomeComponent,
     CaNavbarComponent,
-    CaNavbarLoggedComponent,
-    GoogleMapComponent,
-    CaHomeLoggedComponent,
-    CaRegistrationPromoteComponent,
-    CaNavbarLoggedPromoteComponent,
-    CaHomeLoggedPromoteComponent,
-    CaGestisciAttivitaPromoteComponent,
+  
     
   ],
   imports: [
@@ -45,7 +31,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     MatSlideToggleModule,
     MatSliderModule 
   ],
-  providers: [GoogleMapComponent],
+  providers: [],
   bootstrap: [AppComponent],
   
 })
